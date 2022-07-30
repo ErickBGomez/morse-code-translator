bc983b9e5fd6636082a263856c33b801fefc5c9a
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2022-07-30 12:00:07 -0600

    Starting project

--- MorseCodeTranslator/MorseCodeTranslator.Designer.cs
+++ MorseCodeTranslator/MorseCodeTranslator.Designer.cs
@@ -29,19 +29,30 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MorseCodeTranslator));
+            this.enterInputText = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // enterInputText
+            // 
+            resources.ApplyResources(this.enterInputText, "enterInputText");
+            this.enterInputText.Name = "enterInputText";
+            this.enterInputText.Click += new System.EventHandler(this.label1_Click);
             // 
             // MorseCodeTranslator
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.enterInputText);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MorseCodeTranslator";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label enterInputText;
     }
 }
 
